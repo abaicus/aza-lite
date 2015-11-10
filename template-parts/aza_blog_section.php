55fbdfd099ce362a5a432661faf864e983a201e5
abaicus <baicusandrei@me.com>
2015-11-10 12:52:35 +0200

    latest news section functionality

---
+++ template-parts/aza_blog_section.php
@@ -0,0 +1,54 @@
+<section id="blog">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 text-center">
+                        <h1>Latest News</h1>
+                        <div class="separator"></div>
+
+                        <p>Keep your users in touch with your latest blog posts and updates.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row row-centered">
+                
+                
+                
+	<?php if ( have_posts() ) : ?>
+    <?php query_posts("showposts=5"); ?>
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/blog-posts', get_post_format() );
+				?>
+
+			<?php endwhile; ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+    <?php wp_reset_query(); ?>
+
+		<?php endif; ?>
+
+
+
+ </div>
+            </div>
+
+
+        </section>
---
+++ template-parts/blog-posts.php
@@ -0,0 +1,7 @@
+<div class="col-xs-12 col-lg-4 col-md-4">
+    <a href="#x">
+        <?php the_post_thumbnail() ?>
+        <?php the_title('<h4></h4>'); ?>
+    </a>
+    <p><?php the_excerpt();?>.</p>
+</div>
