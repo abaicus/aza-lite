062343faec863b3f4bcef1f00c3180bc46a0e24e
abaicus <baicusandrei@me.com>
2015-11-13 15:42:07 +0200

    fixed blogposts front-page permalinks

--- template-parts/blog-posts.php
+++ template-parts/blog-posts.php
@@ -1,6 +1,6 @@
 <div class="col-xs-12 col-lg-4 col-md-4">
-    <a href="#">
-        <h4><?php the_title(); ?></h4>
+    <a href="<?php the_permalink() ?>">
+        <h4><?php the_title() ?></h4>
          <?php the_post_thumbnail() ?>
     </a>
     <p><?php the_excerpt();?></p>
