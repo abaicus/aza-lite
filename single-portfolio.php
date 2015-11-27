e3feb1afa4ff50ef0bcbaedcec6c38e2750eaae4
abaicus <baicusandrei@me.com>
2015-11-27 23:00:19 +0200

    Fixed porftolio single page

--- single-portfolio.php
+++ single-portfolio.php
@@ -1,4 +1,5 @@
-<?php
+<div class="blog-content-wrapper">
+    <?php
 /**
  * The template for displaying all single posts.
  *
@@ -8,27 +9,37 @@
  */
 
 get_header(); ?>
+        <div class="container blog-content">
+            <div class="row">
+                <div class="col-md-9">
+                    <div id="primary" class="content-area">
+                        <main id="main" class="site-main" role="main">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+                            <?php while ( have_posts() ) : the_post(); ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
+                                <?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+                                    <?php the_post_navigation(); ?>
 
-			<?php the_post_navigation(); ?>
-
-			<?php
+                                        <?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
 			?>
 
-		<?php endwhile; // End of the loop. ?>
+                                            <?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
+                        </main>
+                        <!-- #main -->
+                    </div>
+                    <!-- #primary -->
+                </div>
+                <div class="col-md-3">
+                    <?php get_sidebar(); ?>
+                </div>
+            </div>
+        </div>
+</div>
 <?php get_footer(); ?>
+    <?php get_footer(); ?>
